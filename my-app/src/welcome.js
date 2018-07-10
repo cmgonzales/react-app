e73cc8e4ed413802c42b8107b1ba28fd410cf18e
Chris Gonzales <cm.gonzales@yahoo.com>
2018-07-09 17:32:36 -0700

    added new

--- my-app/src/welcome.js
+++ my-app/src/welcome.js
@@ -6,6 +6,16 @@ render(){
 
     return(
         <h1>Wecome</h1>
+        import React from 'react';
+import ReactDOM from 'react-dom';
+import Welcome from './welcome';
+import registerServiceWorker from './registerServiceWorker';
+
+
+
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
+registerServiceWorker();
     )
 }
 }
